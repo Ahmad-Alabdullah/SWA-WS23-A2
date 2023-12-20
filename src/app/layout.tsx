228d97232a9a6848332665184204643975516c15
Ahmad Alabdullah <ahmadva2000@gmail.com>
2023-12-20 17:03:16 +0100

    graphql, auth and login page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,30 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Nav from '@/components/Nav';
+import { Outlet } from 'react-router-dom';
+import LoginContext, { LoginProvider } from '@/context/LoginProvider';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Frontend with Next',
   description: 'Created by Team 10',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <LoginProvider>
+          <Nav></Nav>
+          <div>{children}</div>
+        </LoginProvider>
+      </body>
     </html>
-  )
+  );
 }

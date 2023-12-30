05f590a7b219ceff2094b7498cb489956dd8a627
Elena Badliuk <bael1018@h-ka.de>
2023-12-30 01:31:32 +0100

    Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,100 @@
 import {
+    Box,
     Button,
-    Container,
+    Card,
+    CardContent,
+    CardMedia,
+    Grid,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    TextField,
+    Radio,
+    RadioGroup,
     Typography,
   } from '@mui/material';
   import Link from 'next/link';
   
   function Home() {
     return (
-      <Container>
-        <Typography variant="h1" component="h2" gutterBottom>
-          Willkommen zur Bibliothek!
-        </Typography>
-        <Typography variant="body1" paragraph>
-          Entdecke eine Vielzahl von Büchern und Ressourcen in unserer Bibliothek.
-        </Typography>
-        <Link href="/books" passHref>
-          <Button variant="contained" color="primary">
-            Alle Bücher anzeigen
-          </Button>
-        </Link>
-      </Container>
+      <Box sx={{ flexGrow: 1, padding: '2rem' }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Card>
+              <CardMedia
+                component="img"
+                sx={{
+                  width: '100%',
+                  height: '400px',
+                  objectFit: 'cover',
+                  maxHeight: '40vh',
+                }}
+                image="\public\images\library.jpg"
+                alt="Cover Image"
+              />
+              <Typography variant="h5" className="title" sx={{ width: '60%', margin: 'auto', marginBottom: '1rem', alignItems: 'center', justifyContent: 'center' }}>
+              Willkommen in unserem online Buchhandel!
+              </Typography>
+            </Card>
+        </Grid>
+
+          <Grid item xs={6}>
+            <Card>
+            <CardContent style={{ display: 'flex', flexDirection: 'column', height: '250px', alignItems: 'center' }}>
+                <h3>Starte hier Deine Suche</h3>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="Suchen"
+                  label="Suchen"
+                  name="Suchen"
+                  type="text"
+                  autoFocus
+                  style={{ backgroundColor: '#E6F1EE', marginBottom: '10px' }}
+                />
+                <FormControl>
+                  <FormLabel id="row-radio-buttons-group-label"></FormLabel>
+                  <RadioGroup
+                    row
+                    aria-labelledby="row-radio-buttons-group-label"
+                    name="row-radio-buttons-group"
+                    style={{ justifyContent: 'space-between', width: '100%', marginBottom: '40px' }}
+                  >
+                    <FormControlLabel value="nach Titel" control={<Radio />} label="nach Titel" style={{ paddingRight: '40px' }} />
+                    <FormControlLabel value="nach Author" control={<Radio />} label="nach Author" style={{ paddingRight: '40px' }} />
+                    <FormControlLabel value="nach ISBN" control={<Radio />} label="nach ISBN" style={{ paddingRight: '40px' }} />
+                  </RadioGroup>
+                </FormControl>
+                <Button
+                  variant="contained"
+                  style={{ backgroundColor: '#047857' }}
+                >
+                  erweiterte Suche
+                </Button>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={6}>
+            <Card>
+            <CardContent style={{ display: 'flex', flexDirection: 'column', height: '250px', alignItems: 'center', justifyContent: 'center' }}>
+                <h3>Aktuell beliebteste Bücher</h3>
+                <Typography
+                  sx={{ width: '60%', margin: 'auto', marginBottom: '1rem' }}
+                >
+                </Typography>
+                <Link href="/search" passHref>
+                  <Button 
+                    variant="contained" 
+                    style={{ backgroundColor: '#047857' }}
+                    >
+                    Zur Ansicht
+                  </Button>
+                </Link>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
     );
   }
   
